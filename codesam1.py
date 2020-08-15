8f4def3208ab74b78e127034910d144ccb5403ec
Samantha <missdelafuente@gmail.com>
2020-08-15 10:42:27 +0200

    first version

---
+++ codesam1.py
@@ -0,0 +1,17 @@
+print("hello world")
+print("   /I")
+print("  / I")
+print(" /  I")
+print("/___I")
+name=input("What is your name?")
+print("Hi "+name+"!")
+gender=input("Enter 'M' for Male or 'F' for female")
+
+if gender == "f":
+    print("your a party girl")
+    print("your party name is... pg" + name[0])
+if gender == "m":
+    print("your a party boy")
+    print("your party name is... pb" + name[0])
+
+
